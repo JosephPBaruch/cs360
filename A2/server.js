7adac5552e47a4ba1e94a3a5205eaff4b068de36
Joseph Baruch <josephbaruch48@gmail.com>
2025-02-13 16:21:16 -0800

    Initialized and have starting point

---
+++ A2/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Serve the about.html file
+app.get('/bio', (req, res) => {
+  res.sendFile(path.join(__dirname, 'bio.html'));
+});
+
+// Serve the portfolio.html file
+app.get('/blog', (req, res) => {
+  res.sendFile(path.join(__dirname, 'blog.html'));
+});
+
+// Serve the contact.html file
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+// Serve the signin.html file
+app.get('/signin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'photos.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
