eeb21b16b3d812644b97606f67d00260ca28750a
Joseph Baruch <josephbaruch48@gmail.com>
2025-02-28 13:32:13 -0800

    Everything looking good

--- A3/frontend/src/App.tsx
+++ A3/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register'
 import Home from './Home'
+import ProtectedRoute from './ProtectedRoute';
 import './App.css';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
         </Routes>
     </Router>
   );
