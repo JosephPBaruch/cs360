54fd2901e7d98f41c855534f13e90d8d02ec3b4b
Joseph Baruch <josephbaruch48@gmail.com>
2025-03-03 15:16:24 -0800

    allowed hosts

--- A3/backend/backend/settings.py
+++ A3/backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-3miyhchdz826q=yhtudnvzvcc25mu7-^yv8w^n*i8wm+8g09ol
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['joestack.org', '192.168.254.45']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
