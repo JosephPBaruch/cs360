87ed3bb7635d09d52e61889c57017fa118efc5eb
Joseph Baruch <josephbaruch48@gmail.com>
2025-03-02 19:05:43 -0800

    pathing

--- A3/dummy/pipeline.sh
+++ A3/dummy/pipeline.sh
@@ -38,9 +38,9 @@ cleanup() {
 
     # Delete all Kubernetes resources
     kubectl delete -f ingress.yaml
-    kubectl delete -f server_manifests/client_deployment.yaml
+    kubectl delete -f client_deployment.yaml
     kubectl delete -f client_service.yaml
-    kubectl delete -f server_manifests/backend_deployment.yaml
+    kubectl delete -f backend_deployment.yaml
     kubectl delete -f backend_service.yaml
     kubectl delete -f pvc.yaml
 }
