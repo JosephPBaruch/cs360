871afd057e04d7bb0fc67a788e6339e7d16a42ec
Joseph Baruch <josephbaruch48@gmail.com>
2025-03-03 16:25:52 -0800

    logout

--- A3/frontend/src/Home.tsx
+++ A3/frontend/src/Home.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
-import { Container, Typography, TextField, Button, Table, TableBody, TableCell, TableHead, TableRow, IconButton } from '@mui/material';
+import { Container, Typography, TextField, Button, Table, TableBody, TableCell, TableHead, TableRow, IconButton, AppBar, Toolbar } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { Delete } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 const useStyles = makeStyles({
   container: {
@@ -29,6 +30,13 @@ const useStyles = makeStyles({
   table: {
     marginTop: '2rem',
   },
+  appBar: {
+    marginBottom: '2rem',
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
 });
 
 interface User {
@@ -41,6 +49,7 @@ interface User {
 
 function Home() {
   const classes = useStyles();
+  const navigate = useNavigate();
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
@@ -73,75 +82,88 @@ function Home() {
     });
   };
 
+  const logout = () => {
+    localStorage.removeItem('access_token');
+    navigate('/login');
+  };
+
   return (
-    <Container className={classes.container}>
-      <Typography variant="h4" className={classes.title}>User Management</Typography>
-      <form className={classes.form}>
-        <TextField
-          label="First Name"
-          variant="outlined"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          required
-          fullWidth
-        />
-        <TextField
-          label="Last Name"
-          variant="outlined"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          required
-          fullWidth
-        />
-        <TextField
-          label="Email"
-          type="email"
-          variant="outlined"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          fullWidth
-        />
-        <TextField
-          label="Password"
-          type="password"
-          variant="outlined"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          fullWidth
-        />
-        <Button onClick={createUser} type="submit" variant="contained" color="primary" className={classes.button}>
-          Create User
-        </Button>
-      </form>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>First Name</TableCell>
-            <TableCell>Last Name</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Password</TableCell>
-            <TableCell>Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map(user => (
-            <TableRow key={user.id}>
-              <TableCell>{user.firstName}</TableCell>
-              <TableCell>{user.lastName}</TableCell>
-              <TableCell>{user.email}</TableCell>
-              <TableCell>{user.password}</TableCell>
-              <TableCell>
-                <IconButton onClick={() => deleteUser(user.id)} color="secondary">
-                  <Delete />
-                </IconButton>
-              </TableCell>
+    <>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h6">User Management</Typography>
+          <Button color="inherit" onClick={logout}>Logout</Button>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.container}>
+        <Typography variant="h4" className={classes.title}>User Management</Typography>
+        <form className={classes.form}>
+          <TextField
+            label="First Name"
+            variant="outlined"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+            fullWidth
+          />
+          <TextField
+            label="Last Name"
+            variant="outlined"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+            fullWidth
+          />
+          <TextField
+            label="Email"
+            type="email"
+            variant="outlined"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            fullWidth
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            fullWidth
+          />
+          <Button onClick={createUser} type="submit" variant="contained" color="primary" className={classes.button}>
+            Create User
+          </Button>
+        </form>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Password</TableCell>
+              <TableCell>Actions</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Container>
+          </TableHead>
+          <TableBody>
+            {users.map(user => (
+              <TableRow key={user.id}>
+                <TableCell>{user.firstName}</TableCell>
+                <TableCell>{user.lastName}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.password}</TableCell>
+                <TableCell>
+                  <IconButton onClick={() => deleteUser(user.id)} color="secondary">
+                    <Delete />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Container>
+    </>
   );
 }
 
