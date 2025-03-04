38b9cf8638640b2587784428a77dafbe82f990ce
Joseph Baruch <josephbaruch48@gmail.com>
2025-03-03 16:44:56 -0800

    addeduser

--- A3/backend/users/models.py
+++ A3/backend/users/models.py
@@ -6,12 +6,25 @@ class User(models.Model):
     username = models.CharField(max_length=255, unique=True, blank=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
 
     def save(self, *args, **kwargs):
         if not self.username:
             self.username = str(self.id)
         super(User, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class AddedUser(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='added_users')
+    username = models.CharField(max_length=255, unique=True, blank=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
