bd2ab65ca653d945f46ac5eb3abd30f9b0ed16bb
Joseph Baruch <josephbaruch48@gmail.com>
2025-03-03 17:03:26 -0800

    add user

--- A3/backend/users/serializers.py
+++ A3/backend/users/serializers.py
@@ -17,11 +17,12 @@ class LoginSerializer(serializers.Serializer):
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'password')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
+            username=validated_data['username'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
             email=validated_data['email'],
